0522763e28fcd05d26411cbb47938b340f30ebb3
shijiang.luo <shijiang.luo@foxmail.com>
2020-09-19 17:59:19 +0800

    构建项目

---
+++ src/main/java/com/jiang/SpringbootMessageApplication.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMessageApplication.class, args);
+    }
+
+}
